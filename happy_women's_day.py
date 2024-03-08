b2f14ecb6c7ae9f8666fa81d867e8c365d4e1236
Arslanr369 <70293507+arslanr369@users.noreply.github.com>
2024-03-08 11:41:07 +0500

    Update happy_women's_day.py

--- happy_women's_day.py
+++ happy_women's_day.py
@@ -1,9 +1,10 @@
-print('\n'.join
- ([''.join
-   ([('Womens '[(x-y)%8 ]
-    if ((x*0.05)**2*(y*0.01)
-     **3-(x*0.05)**2*(y*0.1)
-      **3<=0 else' ')
-       for x in range(-30,30)])
-        for y in range(15,-15,-1)]))
-print(" Happy Women's Day !!")
+heart_pattern = [
+    ''.join([
+        '♥ ' if ((x * 0.05)**2 + (y * 0.1)**2 - 1)**3 - (x * 0.05)**2 * (y * 0.1)**3 <= 0 else ' '
+        for x in range(-30, 30)
+    ])
+    for y in range(15, -15, -1)
+]
+
+print('\n'.join(heart_pattern))
+print("Happy Women's Day !!")
